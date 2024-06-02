235c3cc426efc9e9a6ce500021b6fde40d6a2517
vaporyhumo <roanvilina@gmail.com>
2024-06-02 01:20:38 -0400

    Add `spec/spec_helper.rb`

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+require 'farseer'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.example_status_persistence_file_path = '.rspec_status'
+  config.disable_monkey_patching!
+  config.profile_examples = 0
+
+  Kernel.srand config.seed
+end
