ad5722f958e9b8d09c0b8f42135967a5d14187b7
vaporyhumo <roanvilina@gmail.com>
2024-06-02 02:06:16 -0400

    Add `Farseer::char_parser` and `Farseer::Maybe`

--- lib/farseer.rb
+++ lib/farseer.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
+require 'muina'
 require 'zeitwerk'
 loader = Zeitwerk::Loader::for_gem
 loader.setup
 
 module Farseer
+  Maybe = Muina::Maybe
+
+  module_function
+
+  def char_parser
+    ->(char, input) {
+      case input[0]
+      when char then Maybe.return(Result.new(input[0], input[1..]))
+      else nil
+      end
+    }
+  end
 end
 
 loader.eager_load
