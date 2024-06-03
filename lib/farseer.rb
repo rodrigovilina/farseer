8acf49810e4d582228bd4db87f2fb64edce4413c
vaporyhumo <roanvilina@gmail.com>
2024-06-03 00:29:51 -0400

    Add `Farseer.any_char_parser`

--- lib/farseer.rb
+++ lib/farseer.rb
@@ -9,13 +9,15 @@ module Farseer
   WS_REGEX = /^(\s*)(.*)$/
   Maybe = Muina::Maybe
 
-  module_function
+  def self.any_char_parser
+    ->(chars, input) { Farseer.__send__(:any_char_parser_helper, chars, input) }.curry
+  end
 
-  def char_parser
+  def self.char_parser
     ->(char, input) { Farseer.__send__(:char_parser_helper, char, input) }.curry
   end
 
-  def ws_parser
+  def self.ws_parser
     ->(input) {
       match = input.match(WS_REGEX)
 
@@ -23,13 +25,22 @@ module Farseer
     }
   end
 
-  def char_parser_helper(char, input)
+  private
+
+  def self.char_parser_helper(char, input)
     case input[0]
     when char then Maybe.return(Result.new(input[0], input[1..]))
     else Maybe.none
     end
   end
-  private :char_parser_helper
+
+  def self.any_char_parser_helper(chars, input)
+    if chars.any? { |char| char == input[0]  }
+      Maybe.return(Result.new(input[0], input[1..]))
+    else
+      Maybe.none
+    end
+  end
 end
 
 loader.eager_load
--- spec/farseer_spec.rb
+++ spec/farseer_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Farseer do
-  describe '#char_parser' do
+  describe '.char_parser' do
     specify do
       char = 'a'
       result = described_class.char_parser.call(char, 'ab').value!
@@ -19,7 +19,27 @@ RSpec.describe Farseer do
     end
   end
 
-  describe '#ws_parser' do
+  describe '.any_char_parser' do
+    let(:chars) { ['a', 'b'] }
+    let(:char) { chars.sample }
+
+    specify do
+      input = char + 'b'
+      result = described_class.any_char_parser.call(chars, input).value!
+      expected = described_class::Result.new(char, 'b')
+
+      expect(result).to eq(expected)
+    end
+
+    specify do
+      input = 'cb'
+      result = described_class.any_char_parser.call(chars, input)
+
+      expect(result).to be_none
+    end
+  end
+
+  describe '.ws_parser' do
     specify do
       result = described_class.ws_parser.call(' a').value!
       expected = described_class::Result.new(' ', 'a')
