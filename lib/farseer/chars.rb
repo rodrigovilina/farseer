ca3acfb13ced2981c8a195fdc63f5d8222edb4a4
vaporyhumo <roanvilina@gmail.com>
2024-06-03 18:59:53 -0400

    Refactor `Farseer::Chars`

--- lib/farseer/chars.rb
+++ lib/farseer/chars.rb
@@ -7,11 +7,13 @@ module Farseer
       freeze
     end
 
+    DIGITS        = new(('0'..'9').to_a)
+    BASIC_MATH_OP = new(['+', '-', '*', '/'])
+
     def parse(input)
-      if @chars.any? { |char| char == input[0] }
-        Maybe.return(Result.new(input[0], input[1..]))
-      else
-        Maybe.none
+      case input[0]
+      when *@chars then Maybe.return(Result.new(input[0], input[1..]))
+      else Maybe.none
       end
     end
   end
